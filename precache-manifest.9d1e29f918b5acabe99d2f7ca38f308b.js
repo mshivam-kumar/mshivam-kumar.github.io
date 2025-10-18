c9a59ce5108389d5fb7158f695aac43b57555be9
mshivam-kumar <svmkumar28@gmail.com>
2025-10-18 23:40:38 +0530

    Initial commit (clean history)

---
+++ precache-manifest.9d1e29f918b5acabe99d2f7ca38f308b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1af4d77c49914219d6e5d4493458b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7ce6c6724e0222b028",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7ce6c6724e0222b028",
+    "url": "/static/js/main.945f37a7.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2d36817a733c88182b99f2ff1f7467fc",
+    "url": "/static/media/huan.2d36817a.png"
+  },
+  {
+    "revision": "1bc0885e7ffe392b2948d87f01cdad01",
+    "url": "/static/media/resume.1bc0885e.pdf"
+  }
+]);
